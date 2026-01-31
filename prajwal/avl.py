d916b45985404ff7252d539aafd2df3251a04f76
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-01-31 17:44:53 +0530

    Bug fixes - Introduced a tree printing method

--- prajwal/avl.py
+++ prajwal/avl.py
@@ -10,25 +10,24 @@ class AVLTree:
         self.root = None
         print("a new tree created")
 
-    def insert(self, value , node:Node=None , isFirstCall=False):
+    def insert(self, value , node:Node=None ):
 
-        if self.root is None:       # if the tree is just Initialized (No root node exist)
-            self.root = Node(value)
-            return self.root
+        if node is None:       # leaf
+            return Node(value)
         
         # print(f"type of left node : {type(node)}")
         
-        if  node is None and not isFirstCall:                         # just a falg to indicate whether this is the first time calling the method
-            self.root = self.insert(value=value , node=self.root , isFirstCall=True )
-            return self.root
+        # if  node is None and not isFirstCall:                         # just a falg to indicate whether this is the first time calling the method
+        #     self.root = self.insert(value=value , node=self.root , isFirstCall=True )
+        #     return self.root
         
-        if not node:
-            return Node(value=value)
+        # if not node:
+        #     return Node(value=value)
 
         if value < node.value:
-            node.left = self.insert(value=value , node=node.left , isFirstCall=True )
+            node.left = self.insert(value=value , node=node.left )
         elif value > node.value:
-            node.right = self.insert(value=value , node=node.right   , isFirstCall=True)
+            node.right = self.insert(value=value , node=node.right )
         else:
             return node
         
@@ -52,7 +51,7 @@ class AVLTree:
             if self.getBalanceStatus(node.right) <= 0: # RR 
                 print("A R-R case has found")
                 return self.leftRot(node)
-            else:
+            else:                               # RL 
                 print("A R-L case has found")
                 node.right = self.rightRot(node.right)
                 return self.leftRot(node)
@@ -100,7 +99,6 @@ class AVLTree:
         z.height = 1 + max(self.getHeight(z.left) ,self.getHeight(z.right))
         y.height = 1 + max(self.getHeight(y.left) ,self.getHeight(y.right))
 
-        print("A right rotation is being performed")
         return y
 
     def leftRot(self , z:Node ): 
@@ -120,6 +118,7 @@ class AVLTree:
         print("a left rotation has been performed")
     
         return y
+
     def getHeight(self , node:Node):
         if not node:
             return 0
@@ -131,13 +130,105 @@ class AVLTree:
             return 0
         balanceFactor = self.getHeight(node.left) - self.getHeight(node.right)
         return balanceFactor
+    
+
+    def printDiagram(self):
+        if self.root is None:
+            print("(empty tree)")
+            return
+
+        lines, *_ = self._buildDiagram(self.root)
+        for line in lines:
+            print(line)
+
+
+    def _buildDiagram(self, node):
+        """
+        Returns:
+            lines: list of strings (ASCII diagram)
+            width: total width of the diagram
+            root_pos: horizontal index of the root value
+        """
+
+        val = str(node.value)
+        val_width = len(val)
+
+        # Leaf node
+        if node.left is None and node.right is None:
+            return [val], val_width, val_width // 2
+
+        # Only left child
+        if node.right is None:
+            left_lines, left_width, left_root = self._buildDiagram(node.left)
+
+            first_line = (left_root + 1) * " " + (left_width - left_root - 1) * "_" + val
+            second_line = left_root * " " + "/" + (left_width - left_root - 1 + val_width) * " "
+
+            shifted_left = [line + val_width * " " for line in left_lines]
+
+            return (
+                [first_line, second_line] + shifted_left,
+                left_width + val_width,
+                left_width + val_width // 2
+            )
+
+        # Only right child
+        if node.left is None:
+            right_lines, right_width, right_root = self._buildDiagram(node.right)
+
+            first_line = val + right_root * "_" + (right_width - right_root) * " "
+            second_line = (val_width + right_root) * " " + "\\" + (right_width - right_root - 1) * " "
+
+            shifted_right = [val_width * " " + line for line in right_lines]
+
+            return (
+                [first_line, second_line] + shifted_right,
+                right_width + val_width,
+                val_width // 2
+            )
+
+        # Two children
+        left_lines, left_width, left_root = self._buildDiagram(node.left)
+        right_lines, right_width, right_root = self._buildDiagram(node.right)
+
+        first_line = (
+            (left_root + 1) * " " +
+            (left_width - left_root - 1) * "_" +
+            val +
+            right_root * "_" +
+            (right_width - right_root) * " "
+        )
+
+        second_line = (
+            left_root * " " + "/" +
+            (left_width - left_root - 1 + val_width + right_root) * " " +
+            "\\" +
+            (right_width - right_root - 1) * " "
+        )
+
+        # Pad subtrees to same height
+        height = max(len(left_lines), len(right_lines))
+        left_lines += [" " * left_width] * (height - len(left_lines))
+        right_lines += [" " * right_width] * (height - len(right_lines))
+
+        merged_lines = [
+            left_lines[i] + val_width * " " + right_lines[i]
+            for i in range(height)
+        ]
+
+        return (
+            [first_line, second_line] + merged_lines,
+            left_width + val_width + right_width,
+            left_width + val_width // 2
+        )
 
 tree0 = AVLTree()
-0
-tree0.insert(value=12)
-tree0.insert(value=10)
-tree0.insert(value=56)
-tree0.insert(value=50)
-tree0.insert(value=122)
-tree0.insert(value=2)
-# print(type(tree0.))'
+
+tree0.root = tree0.insert(12, tree0.root)
+tree0.root = tree0.insert(10, tree0.root)
+tree0.root = tree0.insert(56, tree0.root)
+tree0.root = tree0.insert(50, tree0.root)
+tree0.root = tree0.insert(122, tree0.root)
+tree0.root = tree0.insert(2, tree0.root)
+
+tree0.printDiagram()
