0252ac5808b47e4241414213f7b69c9a18168740
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-01-31 11:49:57 +0530

    Aswin MP's code added & updated Anirudh P's bloom

--- Anirudh/Bloom.py
+++ Anirudh/Bloom.py
@@ -1,52 +1,35 @@
-#include <stdio.h>
-#include <string.h>
+# hash fn.
+def hash1(word):
+    total = 0
+    for ch in word:
+        total += ord(ch)
+    return total % SIZE
 
-#define SIZE 20
-int bloom[SIZE] = {0};
+def hash2(word):
+    total = 1
+    for ch in word:
+        total *= ord(ch)
+    return total % SIZE
 
-int h1(char *word) {
-    return strlen(word) % SIZE;
-}
 
-int h2(char *word) {
-    int sum = 0;
-    for (int i = 0; word[i] != '\0'; i++) sum += word[i];
-    return sum % SIZE;
-}
+def add(word):
+    bloom[hash1(word)] = 1
+    bloom[hash2(word)] = 1
 
-int main() {
-    int choice;
-    char word[30];
 
-    while (1) {
-        printf("\n1.Add 2.Check 3.Show 4.Exit: ");
-        scanf("%d", &choice);
+def check(word):
+    if bloom[hash1(word)] == 1 and bloom[hash2(word)] == 1:
+        print(word, ": May be present")
+    else:
+        print(word, ": Not present")
 
-        if (choice == 4) break;
+#inpuuts
 
-        if (choice == 3) {
-            for (int i = 0; i < SIZE; i++)  
+add("Fr")
+add("Lol")
 
-             printf("%d ", bloom[i]);
-            printf("\n");
-            continue;
-        }
+print("Bloom Filter:", bloom)
 
-        printf("Enter word: ");
-        scanf("%s", word);
-
-        if (choice == 1) {
-            bloom[h1(word)] = 1;
-            bloom[h2(word)] = 1;
-            printf("Added.\n");
-        } 
-        
-        if (choice == 2) {
-            if (bloom[h1(word)] && bloom[h2(word)]) 
-                printf("Result: Maybe yes\n");
-            else 
-                printf("Result: Definitely no\n");
-        }
-    }
-    return 0;
-}
+check("Fr")     # present
+check("Lol")   # present
+check("Bruuh")   # may or may not be present
